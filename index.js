f00bfcd4962b671bcbab5eeb0eb72ee5b4cb8545
kyasuda2003 <kyasuda2003@gmail.com>
2015-06-07 17:45:50 -0700

    adding count.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 ;(function(){
     var _http=require('http'),
+	_url=require('url'),
         _post={},
         _ref=function(obj, num, req, res){
             var __req=req,__res=res,__m_arr=obj,__num=num;
@@ -31,12 +32,22 @@
                 if (_req.method.toLowerCase()=='post'){
                     
                     _req.on('data', function(chunk) {
-                        console.log("Received body data:");
-                        console.log(chunk.toString());
-                        req.body=JSON.parse(chunk);
+			//console.log("Received body data:"+chunk.toString());
+			var __ref1=_url.parse(_req.url),__body;
+			
+			try {
+			    __body=JSON.parse(chunk);
+			}
+			catch(e){
+			    res.writeHead(501, { "Content-Type": "application/json" });
+			    return res.end(JSON.stringify({status:'Invalid body.'}));
+			}
+
+                        req.body=__body;
                         for (var obj in _post){
-                            if (_post.hasOwnProperty(obj)&&
-                                _req.url.indexOf(obj)>-1){
+			    if (_post.hasOwnProperty(obj)&&
+                                (obj.indexOf(__ref1.pathname)>-1)){
+			        console.log("\n__ref1.pathname:"+__ref1.pathname);
                                 _ref(_post[obj], 1, _req, _res);
                             }
                         }

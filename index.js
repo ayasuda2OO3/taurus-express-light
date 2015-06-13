4cac5a7e2459e3c5ef276e1513ad6238518a41a4
kyasuda2003 <kyasuda2003@gmail.com>
2015-06-13 11:00:40 -0700

    Adding static feature.

--- index.js
+++ index.js
@@ -5,8 +5,11 @@
 
 ;(function(){
     var _http=require('http'),
+	_fs=require('fs'),
 	_url=require('url'),
+	_pkg=require('./package.json'),
         _post={},
+	_static={},
         _ref=function(obj, num, req, res){
             var __req=req,__res=res,__m_arr=obj,__num=num;
             __m_arr[__num](__req, __res, function(){
@@ -16,29 +19,47 @@
                 __m_arr[__num+1]
                         &&(_ref(__m_arr,__num+1,__req,__res));
             });
-        };
+        },
+	_msg={logs:{},logfile:_pkg.name+'_'+new Date().toJSON()+'.log'},
+	_log=function(msg){
+	    var now=new Date().toJSON();
+	    _msg.logs[now]=msg;
+	    
+	    _fs.writeFile(__dirname+'/logs/'+_msg.logfile,JSON.stringify(_msg,null,4),function(){
+	        console.log(now+': '+msg);
+	    })
+	    
+	};
     
+ 
     module.exports={
         post:function(){
             var _ref = Array.prototype.slice.call(arguments);
             _post[_ref[0]]=_ref;
             
         },
+	static:function(urlpath, abspath){
+	    _static[urlpath]=_path.resolve(abspath);
+	    
+	},
         listen:function(port){
 
             _http.createServer(function (req, res) {
-                var _req=req,_res=res,_m_arr=[];
-                    
+		var _req=req,_res=res,_m_arr=[],__ref1=_url.parse(_req.url);
+		
+		console.log("\nrequest: "+__ref1.pathname);
+
                 if (_req.method.toLowerCase()=='post'){
                     
                     _req.on('data', function(chunk) {
-			//console.log("Received body data:"+chunk.toString());
-			var __ref1=_url.parse(_req.url),__body;
+			var __body;
 			
+			        
 			try {
 			    __body=JSON.parse(chunk);
 			}
 			catch(e){
+			    _log('http body parsing error: '+e);
 			    res.writeHead(501, { "Content-Type": "application/json" });
 			    return res.end(JSON.stringify({status:'Invalid body.'}));
 			}
@@ -47,13 +68,35 @@
                         for (var obj in _post){
 			    if (_post.hasOwnProperty(obj)&&
                                 (obj.indexOf(__ref1.pathname)>-1)){
-			        console.log("\n__ref1.pathname:"+__ref1.pathname);
-                                _ref(_post[obj], 1, _req, _res);
+                
+			        return _ref(_post[obj], 1, _req, _res);
                             }
                         }
+
+			
                     });
                     
                 }
+
+		if (_req.method.toLowerCase()=='get'){
+
+		    for (var obj in _static){
+			if (_static.hasOwnProperty(obj)&&
+			    (__ref1.pathname.indexOf(obj)==0)){
+			    var namepath=__ref1.pathname.substring(obj.length);
+			    return _fs.readFile(_static[obj]+'/'+namepath, function (err,data) {
+				if (err) {
+				    res.writeHead(404);
+				    _log('GET file error: '+JSON.stringify(err));
+				    return res.end(JSON.stringify({status:'Invalid file.'}));
+				}
+				res.writeHead(200);
+				return res.end(data);
+			    });
+			}
+		    }
+		    
+		}
                 
             }).listen(port); 
         }
